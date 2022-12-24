81d1f28cec649cd3d871b70bfee172b1e989aed6
Toluwanimi <rjayjutolu419@gmail.com>
2022-12-24 11:40:07 +0100

    Add the quiz section

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-// const correctAns = ['A', 'A', 'A', 'A'];
-// const form = document.querySelector('.quiz-form');
+const correctAns = ['A', 'A', 'A', 'A'];
+const form = document.querySelector('.quiz-form');
 const result = document.querySelector('.result');
 
-// form.addEventListener('submit',e => {
-//     e.preventDefault();
-//     let score = 0;
+form.addEventListener('submit',e => {
+    e.preventDefault();
+    let score = 0;
 
-//     const userAns = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
-//     userAns.forEach((answer, index) => {
-//         if(answer === correctAns[index]){
-//             score+= 25;
-//         }
-//     });
-//     scrollTo(0,0);
-//     result.classList.remove('d-none');
+    const userAns = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    userAns.forEach((answer, index) => {
+        if(answer === correctAns[index]){
+            score+= 25;
+        }
+    });
+    scrollTo(0,0);
+    result.classList.remove('d-none');
 
-//     let output = 0;
-//     const timer = setInterval(() => {
-//         result.querySelector('span').textContent = `${output}%`;
-//         if(output === score){
-//             clearInterval(timer);
-//         } else {
-//             output ++;
-//         }
-//     }, 10);
-// });
+    let output = 0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if(output === score){
+            clearInterval(timer);
+        } else {
+            output ++;
+        }
+    }, 10);
+});
